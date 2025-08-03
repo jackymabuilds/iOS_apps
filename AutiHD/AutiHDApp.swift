06462dc49ae95a4dbc08251076ac5f82e7cdc33f
Jacky Ma <ma.jacky32@gmail.com>
2025-08-03 19:56:54 -0400

    Initial Commit

---
+++ AutiHD/AutiHDApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutiHDApp.swift
+//  AutiHD
+//
+//  Created by Jacky Ma on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AutiHDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
